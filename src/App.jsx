27f681c58ce4c3d1fdc971b22edd4b24cfe7f49e
BhargabLinx <bhargabbhuyan001@gmail.com>
2025-06-20 11:35:51 +0000

    Build the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 function App() {
     return (
-        <>
-            <h1 class="text-3xl font-bold underline"> Hello world! </h1>
-        </>
+        <div className="bg-[#F6F7EB] text-[#393E41] h-screen">
+            <header className="text-center my-3">
+                <p className="text-4xl font-extrabold tracking-[5px]">
+                    Flickbox
+                </p>
+                <p className="text-[#393e41bf] mt-1">
+                    Open Source{" "}
+                    <span className="text-[#E94F37]">
+                        Movie Tracking Website
+                    </span>{" "}
+                    for Minimalist
+                </p>
+            </header>
+        </div>
     );
 }
 
